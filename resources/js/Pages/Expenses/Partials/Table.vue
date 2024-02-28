a23588c1ca2572e304c263533bb65fa27a7a7577
Aaron F <amfischer21@gmail.com>
2024-02-28 18:48:26 -0500

    formatting

--- resources/js/Pages/Expenses/Partials/Table.vue
+++ resources/js/Pages/Expenses/Partials/Table.vue
@@ -16,7 +16,6 @@ const pageLinks = computed(() => {
     return links;
 })
 
-
 </script>
 
 <template>
@@ -71,68 +70,68 @@ const pageLinks = computed(() => {
     </div>
 
     <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-b-lg">
-
     
-    <div class="flex items-center justify-between bg-white p-4 sm:px-6">
-        <div class="flex flex-1 justify-between sm:hidden">
-            <Link 
-                :as="expenses.prev_page_url ? 'a' : 'span'"
-                :href="expenses.prev_page_url"
-                class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
-                Previous
-            </Link>
-            <Link
-                :as="expenses.next_page_url ? 'a' : 'span'"
-                :href="expenses.next_page_url" 
-                class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
-                Next
-            </Link>
-        </div>
-        <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-            <div>
-                <p class="text-sm text-gray-700">
-                    Showing
-                    <span class="font-medium">{{ expenses.from }}</span>
-                    to
-                    <span class="font-medium">{{ expenses.to }}</span>
-                    of
-                    <span class="font-medium">{{ expenses.total }}</span>
-                    results
-                </p>
+        <div class="flex items-center justify-between bg-white p-4 sm:px-6">
+            <div class="flex flex-1 justify-between sm:hidden">
+                <Link 
+                    :as="expenses.prev_page_url ? 'a' : 'span'"
+                    :href="expenses.prev_page_url"
+                    class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
+                    Previous
+                </Link>
+                <Link
+                    :as="expenses.next_page_url ? 'a' : 'span'"
+                    :href="expenses.next_page_url" 
+                    class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">
+                    Next
+                </Link>
             </div>
-            <div>
-                <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-                    <Link 
-                        :as="expenses.prev_page_url ? 'a' : 'span'"
-                        :href="expenses.prev_page_url"
-                        class="relative inline-flex items-center rounded-l-md px-2 py-2 ring-1 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                        :class="expenses.prev_page_url ? 'text-gray-500 ring-gray-300' : 'text-gray-300 ring-gray-200'">
-                        <span class="sr-only">Previous</span>
-                        <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
-                    </Link>
+            <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                <div>
+                    <p class="text-sm text-gray-700">
+                        Showing
+                        <span class="font-medium">{{ expenses.from }}</span>
+                        to
+                        <span class="font-medium">{{ expenses.to }}</span>
+                        of
+                        <span class="font-medium">{{ expenses.total }}</span>
+                        results
+                    </p>
+                </div>
+                <div>
+                    <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                        <Link 
+                            :as="expenses.prev_page_url ? 'a' : 'span'"
+                            :href="expenses.prev_page_url"
+                            class="relative inline-flex items-center rounded-l-md px-2 py-2 ring-1 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                            :class="expenses.prev_page_url ? 'text-gray-500 ring-gray-300' : 'text-gray-300 ring-gray-200'">
+                            <span class="sr-only">Previous</span>
+                            <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
+                        </Link>
 
-                    <Link 
-                        v-for="link in pageLinks" 
-                        :href="link.url" 
-                        aria-current="page"
-                        class="relative inline-flex items-center px-4 py-2 text-sm font-semibold"
-                        :class="link.active ? 'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' : 'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0'">
-                        {{ link.label }}
-                    </Link>
-                    <!-- <span class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">...</span> -->
+                        <Link 
+                            v-for="link in pageLinks" 
+                            :href="link.url" 
+                            aria-current="page"
+                            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold"
+                            :class="link.active ? 'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' : 'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0'">
+                            {{ link.label }}
+                        </Link>
+                        <!-- <span class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">...</span> -->
 
-                    <Link 
-                        :as="expenses.next_page_url ? 'a' : 'span'"
-                        :href="expenses.next_page_url"
-                        class="relative inline-flex items-center rounded-r-md px-2 py-2 ring-1 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-                        :class="expenses.next_page_url ? 'text-gray-500 ring-gray-300' : 'text-gray-300 ring-gray-200'">
-                        <span class="sr-only">Next</span>
-                        <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
-                    </Link>
-                </nav>
+                        <Link 
+                            :as="expenses.next_page_url ? 'a' : 'span'"
+                            :href="expenses.next_page_url"
+                            class="relative inline-flex items-center rounded-r-md px-2 py-2 ring-1 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                            :class="expenses.next_page_url ? 'text-gray-500 ring-gray-300' : 'text-gray-300 ring-gray-200'">
+                            <span class="sr-only">Next</span>
+                            <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
+                        </Link>
+                    </nav>
+                </div>
             </div>
         </div>
+
     </div>
-</div>
 
 </template>
