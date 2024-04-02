7f60b337064048c3913965e5d83bddb66332aef9
Aaron F <amfischer21@gmail.com>
2024-04-02 11:13:52 -0500

    backup config update

--- config/backup.php
+++ config/backup.php
@@ -8,7 +8,7 @@ return [
          * The name of this application. You can use this name to monitor
          * the backups.
          */
-        'name'                                => 'et-backup',
+        'name'                                => 'app-backups',
 
         'source'                              => [
 
@@ -249,7 +249,7 @@ return [
      */
     'monitor_backups' => [
         [
-            'name'          => 'et-backup',
+            'name'          => 'app-backups',
             'disks'         => ['s3'],
             'health_checks' => [
                 \Spatie\Backup\Tasks\Monitor\HealthChecks\MaximumAgeInDays::class          => 1,
