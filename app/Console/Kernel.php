f8ed1a360b8ee05e48700851138c41e2ebe565f4
Aaron F <amfischer21@gmail.com>
2024-04-02 11:45:28 -0500

    schedule setup - add backup jobs

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,6 +13,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('backup:run')->weeklyOn(1, '6:30');
+        $schedule->command('backup:clean')->weeklyOn(1, '6:30');
     }
 
     /**
