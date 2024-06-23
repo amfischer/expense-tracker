630d0c1e3f3f9585d5605f794a3d4156a362a6af
Aaron F <amfischer21@gmail.com>
2024-06-23 07:44:32 -0500

    Bugfix - alerts now show and disappear after 3 seconds

--- resources/js/Stores/alert.js
+++ resources/js/Stores/alert.js
@@ -1,16 +1,19 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
-import { router } from '@inertiajs/vue3';
+// import { router } from '@inertiajs/vue3';
 
 export const useAlertStore = defineStore('alert', () => {
     const message = ref('');
 
-    const setMessage = (msg) => (message.value = msg);
+    const setMessage = (msg) => {
+        message.value = msg;
+        setTimeout(() => clear(), 3000);
+    };
 
     const clear = () => (message.value = '');
 
     // clear alert if navigating to a new page
-    router.on('finish', () => clear());
+    // router.on('finish', () => clear());
 
     return { message, setMessage, clear };
 });
