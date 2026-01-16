5387a2045d3d47c554dd28fbe1b04836f001e960
Shift <shift@laravelshift.com>
2026-01-16 18:44:56 +0000

    Streamline config files

--- config/sanctum.php
+++ config/sanctum.php
@@ -15,10 +15,11 @@ return [
     |
     */
 
-    'stateful'     => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort()
+        Sanctum::currentApplicationUrlWithPort(),
+        // Sanctum::currentRequestHost(),
     ))),
 
     /*
@@ -33,7 +34,7 @@ return [
     |
     */
 
-    'guard'        => ['web'],
+    'guard' => ['web'],
 
     /*
     |--------------------------------------------------------------------------
@@ -46,7 +47,7 @@ return [
     |
     */
 
-    'expiration'   => null,
+    'expiration' => null,
 
     /*
     |--------------------------------------------------------------------------
@@ -74,10 +75,10 @@ return [
     |
     */
 
-    'middleware'   => [
+    'middleware' => [
         'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
-        'encrypt_cookies'      => App\Http\Middleware\EncryptCookies::class,
-        'verify_csrf_token'    => App\Http\Middleware\VerifyCsrfToken::class,
+        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
+        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
 
 ];
