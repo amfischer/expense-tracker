733c08df5b2387d4a9822bd116df2dbea605b470
Aaron F <amfischer21@gmail.com>
2026-01-19 10:48:53 -0500

    add hourly inspire cmd for debugging

--- routes/console.php
+++ routes/console.php
@@ -8,6 +8,6 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-// Schedule::command('inspire')->hourly();
+Schedule::command('inspire')->hourly();
 Schedule::command('backup:run')->weeklyOn(1, '6:30');
 Schedule::command('backup:clean')->weeklyOn(1, '6:30');
