2013bd12e22b0ed0bd92867c706a749750cbee84
Aaron F <amfischer21@gmail.com>
2026-06-07 20:54:07 -0500

    fix expense table filter and show child categories

--- resources/js/Components/Tables/FilterOption.vue
+++ resources/js/Components/Tables/FilterOption.vue
@@ -17,12 +17,12 @@ const filter = computed(() => props.scout.form.filters[props.field] || []);
     <Disclosure as="div" class="border-t border-gray-200 px-4 py-6" v-slot="{ open }">
         <DisclosureButton
             as="h3"
-            class="flex w-full items-center justify-between bg-white text-sm text-gray-400 cursor-pointer">
+            class="flex w-full cursor-pointer items-center justify-between bg-white text-sm text-gray-400">
             <div class="flex items-center">
                 <span class="font-medium text-gray-900">{{ title }}</span>
                 <span
                     v-if="filter.length > 0"
-                    class="ml-1.5 rounded-sm bg-gray-200 px-1.5 py-0.5 text-xs font-semibold tabular-nums text-gray-700">
+                    class="ml-1.5 rounded-sm bg-gray-200 px-1.5 py-0.5 text-xs font-semibold text-gray-700 tabular-nums">
                     {{ filter.length }}
                 </span>
             </div>
@@ -30,21 +30,36 @@ const filter = computed(() => props.scout.form.filters[props.field] || []);
                 <ChevronDownIcon :class="[open ? '-rotate-180' : 'rotate-0', 'h-5 w-5 transform']" aria-hidden="true" />
             </span>
         </DisclosureButton>
-        <DisclosurePanel class="pt-6 space-y-6">
+        <DisclosurePanel class="space-y-6 pt-6">
             <template v-for="option in options" :key="option.id">
                 <div class="flex items-center" @click="scout.toggleArrayFilter(field, option.id)">
                     <input
                         :id="`filter-mobile-${field}-${option.id}`"
                         :checked="scout.form.filters[field]?.includes(`${option.id}`)"
                         type="checkbox"
-                        class="h-4 w-4 rounded-sm border-gray-300 text-indigo-600 focus:ring-indigo-500 cursor-pointer" />
+                        class="h-4 w-4 cursor-pointer rounded-sm border-gray-300 text-indigo-600 focus:ring-indigo-500" />
                     <label
                         @click.prevent
                         :for="`filter-mobile-${field}-${option.id}`"
-                        class="ml-3 text-sm text-gray-500 cursor-pointer">
+                        class="ml-3 cursor-pointer text-sm text-gray-500">
                         {{ option.name }}
                     </label>
                 </div>
+                <template v-for="child in option.children" :key="child.id">
+                    <div class="flex items-center" @click="scout.toggleArrayFilter(field, child.id)">
+                        <input
+                            :id="`filter-mobile-${field}-${child.id}`"
+                            :checked="scout.form.filters[field]?.includes(`${child.id}`)"
+                            type="checkbox"
+                            class="h-4 w-4 cursor-pointer rounded-sm border-gray-300 text-indigo-600 focus:ring-indigo-500" />
+                        <label
+                            @click.prevent
+                            :for="`filter-mobile-${field}-${child.id}`"
+                            class="ml-3 cursor-pointer text-sm text-gray-500">
+                            {{ child.name }}
+                        </label>
+                    </div>
+                </template>
             </template>
         </DisclosurePanel>
     </Disclosure>
