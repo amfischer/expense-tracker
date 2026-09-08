ca3a4577ed9651e4a81af10166bf5ca452858111
Shift <shift@laravelshift.com>
2026-09-08 21:50:33 +0000

    Default config files
    In an effort to make upgrading the constantly changing config files easier, Shift defaulted them and merged your true customizations - where ENV variables may not be used.

--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default'  => env('MAIL_MAILER', 'log'),
+    'default' => env('MAIL_MAILER', 'log'),
 
     /*
     |--------------------------------------------------------------------------
@@ -35,25 +35,25 @@ return [
     |
     */
 
-    'mailers'  => [
+    'mailers' => [
 
-        'smtp'       => [
-            'transport'    => 'smtp',
-            'scheme'       => env('MAIL_SCHEME'),
-            'url'          => env('MAIL_URL'),
-            'host'         => env('MAIL_HOST', '127.0.0.1'),
-            'port'         => env('MAIL_PORT', 2525),
-            'username'     => env('MAIL_USERNAME'),
-            'password'     => env('MAIL_PASSWORD'),
-            'timeout'      => null,
+        'smtp' => [
+            'transport' => 'smtp',
+            'scheme' => env('MAIL_SCHEME'),
+            'url' => env('MAIL_URL'),
+            'host' => env('MAIL_HOST', '127.0.0.1'),
+            'port' => env('MAIL_PORT', 2525),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
             'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
         ],
 
-        'ses'        => [
+        'ses' => [
             'transport' => 'ses',
         ],
 
-        'postmark'   => [
+        'postmark' => [
             'transport' => 'postmark',
             // 'message_stream_id' => env('POSTMARK_MESSAGE_STREAM_ID'),
             // 'client' => [
@@ -61,27 +61,27 @@ return [
             // ],
         ],
 
-        'resend'     => [
+        'resend' => [
             'transport' => 'resend',
         ],
 
-        'sendmail'   => [
+        'sendmail' => [
             'transport' => 'sendmail',
-            'path'      => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
+            'path' => env('MAIL_SENDMAIL_PATH', '/usr/sbin/sendmail -bs -i'),
         ],
 
-        'log'        => [
+        'log' => [
             'transport' => 'log',
-            'channel'   => env('MAIL_LOG_CHANNEL'),
+            'channel' => env('MAIL_LOG_CHANNEL'),
         ],
 
-        'array'      => [
+        'array' => [
             'transport' => 'array',
         ],
 
-        'failover'   => [
-            'transport'   => 'failover',
-            'mailers'     => [
+        'failover' => [
+            'transport' => 'failover',
+            'mailers' => [
                 'smtp',
                 'log',
             ],
@@ -89,8 +89,8 @@ return [
         ],
 
         'roundrobin' => [
-            'transport'   => 'roundrobin',
-            'mailers'     => [
+            'transport' => 'roundrobin',
+            'mailers' => [
                 'ses',
                 'postmark',
             ],
@@ -110,21 +110,10 @@ return [
     |
     */
 
-    'from'     => [
+    'from' => [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name'    => env('MAIL_FROM_NAME', 'Example'),
+        'name' => env('MAIL_FROM_NAME', env('APP_NAME', 'Laravel')),
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Markdown Mail Settings
-    |--------------------------------------------------------------------------
-    |
-    | If you are using Markdown based email rendering, you may configure your
-    | theme and component paths here, allowing you to customize the design
-    | of the emails. Or, you may simply stick with the Laravel defaults.
-    |
-    */
 
     'markdown' => [
         'theme' => env('MAIL_MARKDOWN_THEME', 'default'),
